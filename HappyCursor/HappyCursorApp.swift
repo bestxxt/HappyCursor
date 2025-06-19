056524e5e7996ad2c66002651e43fa8fb05f6722
bestxxt <scauxxt@gmail.com>
2025-06-18 20:52:04 -0400

    Initial Commit

---
+++ HappyCursor/HappyCursorApp.swift
@@ -0,0 +1,17 @@
+//
+//  HappyCursorApp.swift
+//  HappyCursor
+//
+//  Created by 谢鑫涛 on 2025-06-18.
+//
+
+import SwiftUI
+
+@main
+struct HappyCursorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HappyCursorTests/HappyCursorTests.swift
@@ -0,0 +1,17 @@
+//
+//  HappyCursorTests.swift
+//  HappyCursorTests
+//
+//  Created by 谢鑫涛 on 2025-06-18.
+//
+
+import Testing
+@testable import HappyCursor
+
+struct HappyCursorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
